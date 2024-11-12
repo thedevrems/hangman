aaad099af417dfdfbdf2d539591a59ec1d9ef6e8
gremy <remy.guibert@ynov.com>
2024-11-13 00:17:36 +0100

    Correctif Bug with letter ç & Correctif Bug For English Hard Difficulty

--- managegame/isletter.go
+++ managegame/isletter.go
@@ -8,7 +8,7 @@ package managegame
 // Return :
 // The function will return a boolean that will return whether or not the character is OK
 func IsLetter(s rune) bool {
-	if (s >= 65 && s <= 90) || (s >= 97 && s <= 122) || (s >= 192 && s <= 197) || (s >= 199 && s <= 207) || (s >= 210 && s <= 214) || (s >= 217 && s <= 221) || (s >= 224 && s <= 229) || (s >= 232 && s <= 235) || (s >= 249 && s <= 252) {
+	if (s >= 65 && s <= 90) || (s >= 97 && s <= 122) || (s >= 192 && s <= 197) || (s >= 199 && s <= 207) || (s >= 210 && s <= 214) || (s >= 217 && s <= 221) || (s >= 224 && s <= 229) || (s >= 231 && s <= 235) || (s >= 249 && s <= 252) {
 		return true
 	} else {
 		return false
